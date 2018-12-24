5d119e87bb733ff74d40f2f2c54415ba30329c02
Louis Wyborn <louiswyborn@gmail.com>
2018-12-24 14:57:33 +0000

    Added abilioty to view, edit and delete notes

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -6,6 +6,7 @@ import Login from "./containers/Login";
 import AppliedRoute from "./components/AppliedRoute";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
 export default ({ childProps }) =>
   <Switch>
@@ -13,6 +14,7 @@ export default ({ childProps }) =>
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
     <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
