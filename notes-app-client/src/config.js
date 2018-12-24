28708a53c7c02545ea2800c6af47d6dbca3350f6
Louis Wyborn <louiswyborn@gmail.com>
2018-12-24 12:48:54 +0000

    Added AWS connection backend

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-1",
+    BUCKET: "ahmerb-notesapp-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-west-1",
+    URL: "https://epo4si2417.execute-api.eu-west-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_feTC7GHwR",
+    APP_CLIENT_ID: "6kgt37q6rotvjflkf11qjlsi85",
+    IDENTITY_POOL_ID: "eu-west-1:0b73f0f5-1316-41bc-b757-7dc5603135a9"
+  }
+};
