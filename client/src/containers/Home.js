37659985d04cccdc1ece5bd1af70def2ece000e2
Ahmer Butt <ab15015@bristol.ac.uk>
2019-01-03 14:52:51 +0000

    add login/signup buttons to HomePage and add npm deploy scripts

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -3,6 +3,7 @@ import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import "./Home.css";
 import { API } from "aws-amplify";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
 export default class Home extends Component {
   constructor(props) {
@@ -62,8 +63,16 @@ export default class Home extends Component {
   renderLander() {
     return (
       <div className="lander">
-        <h1>Spirit Dogs</h1>
-        <p>Which dog are you today?</p>
+        <h1>Scratch</h1>
+        <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
